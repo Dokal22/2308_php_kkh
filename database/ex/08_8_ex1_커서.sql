34b7cc1d5012c40a6fccbf87c7a14c5498fdd802
Dokal22 <stly3466@gmail.com>
2023-09-07 22:26:25 +0900

    0907 복습

--- database/ex/08_8_ex1_커서.sql
+++ database/ex/08_8_ex1_커서.sql
@@ -8,3 +8,44 @@ CURSOR
 	
 	
 	레코드 값을 순차적이나 특정적으로 가져와서 쓰고싶어서 생긴게 커선가?
+
+
+-- (Cursor) 
+한 행씩 처리하기 위한 방식
+
+-- 구조
+	DELIMITER $$
+	CREATE PROCEDURE test()
+	BEGIN
+	DECLARE sal INT;
+	DECLARE sum_sal INT;
+	DECLARE cur_sal INT;
+	DECLARE end_row BOOLEAN DEFAULT FALSE;
+
+	-- 선언
+	DECLARE cur_sal CURSOR FOR
+		SELECT salary FROM salaries WHERE emp_no = 10001;
+
+	-- 행이 끝이면 end_row에 true 대입
+	DECLARE CONTINUE HANDLER FOR NOT FOUND
+		SET end_row = TRUE;
+
+	-- 커서 오픈
+	OPEN cur_sal;
+
+	-- 루프 시작
+	cursor_loop: LOOP
+		-- 
+		FETCH cur_sal INTO sal;
+
+		-- 행이 긑이면 루프 종료
+		IF end_row THEN
+			LEAVE cursor_loop;
+		END IF;
+		
+		SET sum_sal = sum_sal + sal;
+	END LOOP cursor_loop;
+
+	SELECT sum_sal;
+	END $$
+	DELIMITER ;
--- database/ex/08_8_ex1_트리거.sql
+++ database/ex/08_8_ex1_트리거.sql
@@ -1,2 +1,38 @@
-트리거 
-	
+트리거(Trigger
+이벤트(update, insert, delete)가 발생했을 때 발동
+(자기가 직접 실행 불가)
+ IN, OUT 매개 변수 없음
+
+-- 실행 시기
+AFTER 
+BEFORE
+
+--  임시테이블
+OLD - 작동 전
+NEW - 작동 후
+
+-- 생성
+DELIMITER $$
+CREATE TRIGGER 트리거명
+{BEFORE | AFTER} {INSERT | UPDATE| DELETE } -- 실행시기와 작업 선택
+ON 테이블 -- 트리거를 부착할 테이블
+FOR EACH ROW -- 아래 나올 조건에 해당하는 모든 row에 적용
+
+BEGIN
+트리거시 실행되는 코드
+IF NEW.컬럼 != OLD.컬럼 THEN -- update 트리거는 old와 new 값이 존재한다.
+UPDATE 테이블
+SET 컬럼 = NEW.컬럼
+WHERE 컬럼 = OLD.컬럼;
+END IF;
+END $$
+DELIMITER ;
+
+-- 실행
+설정한 쿼리를 실행하면 자동작동
+
+-- 트리거 확인
+SHOW TRIGGERS;
+
+-- 삭제
+DROP TRIGGER 트리거명; 
