36cad7e109c686a7bf13ae61067e6028db684bc2
Dokal22 <stly3466@gmail.com>
2023-09-13 22:06:08 +0900

    0913 뽀씁뽀씁

--- php/tng/02_064_펑션.php
+++ php/tng/02_064_펑션.php
@@ -20,6 +20,7 @@
 // echo jgw(10000);
 
 
+// 교재용 재귀함수
 // function recurs($i) {
 // 	if($i===0){
 // 		return 0;
@@ -29,31 +30,44 @@
 // 	}
 // 	return $i + recurs($i-1);
 // }
-// echo recurs(0);
+// echo recurs(12);
 
 
-function a($a){
-	settype($a,"int");
-	$c=0;
-	$d=0;
-	for($b=strlen($a);$b>0;$b--){ 		// 4 	3 	2
-		$c=floor($a/pow(10,($b-1)));  	// 4	5	
-		$a-=$c*pow(10,($b-1));  		// 565	65
-		$d+=$c;							// +4	+5
-	}
-	return $d;
-}
-echo a("4565");
+// function a($a){
+// 	settype($a,"int");
+// 	$c=0;
+// 	$d=0;
+// 	for($b=strlen($a);$b>0;$b--){ 		// 4 	3 	2
+
+// 		$c=floor($a/pow(10,($b-1)));  	// 4	5	
+// 		$a-=$c*pow(10,($b-1));  		// 565	65
+// 		$d+=$c;							// +4	+5
+
+// 	}
+// 	return $d;
+// }
+// echo a("4565");
 // echo floor(4565/pow(10,(4-1)));  	// 4	5	
 // $a-=$c*pow(10,($b-1));  // 565	65
 
 // 쌤방식
-// for($idx=0;$idx<=$len-1;$idx++){
-// 	$sum+=(int)mb_substr($str,$idx,1);
+// function my_test(string $str) {
+// 	$sum=0;
+// 	$len = mb_strlen($str);
+// 	for($idx=0;$idx<=$len-1;$idx++){
+// 		$sum+=(int)mb_substr($str,$idx,1);
+// 	}
+// 	return $sum;
 // }
+// echo my_test("738291");
 
 // 젤빠른게 어레이섬(php에서 만들어논거 쓰세요)
-// (=return array_sum($arr));
+// <return array_sum($arr)>
+// function a($a){
+// 	$b=str_split($a);
+// 	return array_sum($b);
+// }
+// echo a("928374");
 
 	
 // 학생분들꺼
