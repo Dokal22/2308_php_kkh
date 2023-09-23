60a046197535d587d095644ab8267cd5c41b26a7
Dokal22 <stly3466@gmail.com>
2023-09-23 23:23:22 +0900

    0923 숙제

--- side_project/mini_board/src/header.php
+++ side_project/mini_board/src/header.php
@@ -1,6 +1,28 @@
+
+<div class="never_img">never</div>
+<div class="header_flex">
+        <a class="header_a" href="#">카페홈</a>
+        <div class="sero"></div>
+        <a class="header_a" href="#">이웃</a>
+        <div class="sero"></div>
+        <a class="header_a" href="#">가입카페</a>
+        <div class="sero"></div>
+        <a class="header_a" href="#">새글</a>
+        <div class="sero"></div>
+        <a class="header_a" href="#">내소식</a>
+        <div class="sero"></div>
+        <a class="header_a" href="#">채팅</a>
+        <div class="sero"></div>
+        <a class="header_a" href="#">나▼</a>
+        <a class="header_a" href="#">💬</a>
+        <a class="header_a" href="#">▦</a>
+</div>
+
+
+
 <header>
        
-        <h1>mini Board🐱‍👤</h1>
+        <h1>치얼쓰🍷</h1>
 
 </header>
 
--- side_project/mini_board/src/list.php
+++ side_project/mini_board/src/list.php
@@ -11,7 +11,7 @@ require_once(ROOT."lib/lib_db.php");
 
 $list_cnt=5; // 한 페이지 최대 표시 수
 $page_num=1; // 페이지 번호 초기화
-
+$page_list=10;
 
 
 
@@ -82,80 +82,131 @@ try{
     <title>리스트 페이지</title>
 </head>
 <body>
+    <div class="baggat">
+        <?php
+            require_once(FILE_HEADER);
+        ?>
 
-    <?php
-        require_once(FILE_HEADER);
-    ?>
+        <form class="search" action="">
+        <!-- <form class="search" action="/mini_board/src/detail.php/?search= echo $search;"> -->
+            <input type="text" name="search" id="search">
+            <button type="submit">검색</button>
+        </form>
 
-    <main>
+        <main>
 
-        <table>
+            <aside>
+                길쭉하게 옆에 달려있기~
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            </aside>
 
-            <colgroup>
-                <col width=10%>
-                <col width=70%>
-                <col width=20%>
-            </colgroup>
+            <div class="contents">
 
+                <h3>자유게시판</h3>
+                <a href="#" class="favorate">★</a>
 
-            <tr class="table-title">
-                <th>번호</th>
-                <th>제목</th>
-                <th>
-                    작성일자 
-                    <a href="/mini_board/src/insert.php" id="insert" class="page-btn">작성</a>
-                </th>
-            </tr>
+                <div class="sort">
+                    <a href="" class="sort_a">새글 구독</a>
+                    <div class="slider"></div>
+                    <!-- <input type="range" id="gudok" name="gudok" min="0" max="1" step="1" style="width:25px;"> -->
+                    <a href="" class="sort_a">□ 공지 숨기기</a>
+                    <div class="sero"></div>
+                    <a href="" class="sort_a">▤</a>
+                    <a href="" class="sort_a">▦</a>
+                    <a href="" class="sort_a">📄</a>
+                    <select name="gaeshick" id="gaeshick">
+                        <option value="5">5개씩</option>
+                        <option value="10">10개씩</option>
+                        <option value="15">15개씩</option>
+                        <option value="20">20개씩</option>
+                        <option value="30">30개씩</option>
+                        <option value="40">40개씩</option>
+                        <option value="50">50개씩</option>
+                    </select>
+                </div>
 
-            
- 
-            <?php foreach($result as $item){ ?>
+                <table>
 
-                <tr>
-                    <td><?php echo $item["id"]; ?></td>
-                    <td><a href="/mini_board/src/detail.php/?id=<?php echo $item["id"]; ?>&page=<?php echo $page_num; ?>">
-                        <?php echo $item["title"]; ?></a></td>
-                    <td><?php echo $item["create_at"]; ?></td>
-                </tr>
-
-            <?php } ?>
-
-            
-        </table>
-        <section>
-
-        <div class="tri">
-        <a class="page-btn" id="page-btn-L" href="http://localhost/mini_board/src/list.php/?page=<?php echo $prev_page_num ?>"></a>
-        </div>
-                <?php
-                // $culc=(((ceil($page_num/5))-1)*5);
-                // $i=$culc+1;
-                //         for($i;$i<=$culc+5;$i++){
-                //             if($i>$max_page_num){   
-                //             break;}
-                $page_list=5;
-                $culc=((ceil($page_num/$page_list))*$page_list);
-                $i=$culc-($page_list-1);
-                        for($i;$i<=$culc;$i++){
-                            if($i>$max_page_num){   
-                            break;}
-                ?>        
-                    <a class="page-btn" id="page-btn" href="http://localhost/mini_board/src/list.php/?page=<?php echo $i; ?>"><?php echo $i; ?></a>
-                <?php
-                }
-                ?>
+                    <colgroup>
+                        <col width=10%>
+                        <col width=50%>
+                        <col width=14%>
+                        <col width=10%>
+                        <col width=8%>
+                        <col width=8%>
+                    </colgroup>
 
 
-        <div class="tri">
-        <a class="page-btn" id="page-btn-R" href="http://localhost/mini_board/src/list.php/?page=<?php echo $next_page_num ?>"></a>
-        </div>
-            
+                    <tr class="table-title">
+                        <th>번호</th>
+                        <th>제목</th>
+                        <th>작성자</th>
+                        <th>작성일자</th>
+                        <th>조회</th>
+                        <th>좋아요</th>
+                    </tr>
 
-        </section>
-
+                    
         
+                    <?php foreach($result as $item){ ?>
 
-    </main>
+                        <tr>
+                            <td><?php echo $item["id"]; ?></td>
+                            <td><a href="/mini_board/src/detail.php/?id=<?php echo $item["id"]; ?>&page=<?php echo $page_num; ?>">
+                                <?php echo $item["title"]; ?></a></td>
+                            <td><?php
+                                     if(date("Y-m-d") == substr($item["create_at"],0,10)){
+                                        echo substr($item["create_at"],10,6);
+                                    }
+                                     else {echo substr($item["create_at"],0,10);}
+                                 ?></td>
+                        </tr>
+
+                    <?php } ?>
+
+                    
+                </table>
+                
+                <a href="/mini_board/src/insert.php" id="insert" class="jagseong"><div class="pen">✏</div>글쓰기</a>
+
+                <section>
+
+                    <div class="page_buts">
+                        <div class="tri">
+                            <a class="page-btn" id="page-btn-L" href="http://localhost/mini_board/src/list.php/?page=<?php echo $prev_page_num ?>"></a>
+                        </div>
+                            <?php
+                                // $culc=(((ceil($page_num/5))-1)*5);
+                                // $i=$culc+1;
+                                //         for($i;$i<=$culc+5;$i++){
+                                //             if($i>$max_page_num){   
+                                //             break;}
+                                $culc=((ceil($page_num/$page_list))*$page_list);
+                                $i=$culc-($page_list-1);
+                                        for($i;$i<=$culc;$i++){
+                                            if($i>$max_page_num){   
+                                            break;}
+                            ?> 
+
+                            <a class="page-btn" id="page-btn" href="http://localhost/mini_board/src/list.php/?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                            
+                            <?php
+                                }
+                            ?>
+
+
+                        <div class="tri">
+                            <a class="page-btn" id="page-btn-R" href="http://localhost/mini_board/src/list.php/?page=<?php echo $next_page_num ?>"></a>
+                        </div>                  
+                    </div>  
+
+                </section>
+
+            </div>            
+
+        </main>
+
+    </div>
 
 </body>
 </html>
