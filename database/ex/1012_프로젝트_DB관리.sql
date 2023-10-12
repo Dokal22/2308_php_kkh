a3b6825f3ed522a2265b738213dcba1200203c65
Dokal22 <stly3466@gmail.com>
2023-10-12 18:08:07 +0900

    1012 kkh

---
+++ database/ex/1012_프로젝트_DB관리.sql
@@ -0,0 +1,67 @@
+FLUSH PRIVILEGES;
+
+ROLLBACK;
+
+SELECT 
+	 	 COUNT(id) cnt 
+	 FROM 
+	 	 boards
+	 where
+		 delete_flg = 0
+		 and id > 63
+		 ;
+		 
+		 
+		 
+ select 
+			j.id 
+			,j.item_name 
+			,j.amount 
+			,j.d_day 
+			,t.tag_img 
+		 from 
+			 jang j 
+		 	join 
+			  tag_type t 
+		  on 
+		     j.tag_id = t.tag_id 
+		 where 
+		     j.finished = 0 
+		 order by 
+		     j.d_day desc 
+		     ,j.id desc 	
+			  ;	 
+			  
+			  update 
+			jang 
+		 set 
+		  	finished = '1' 
+		  	,finished_at = date(NOW()) 
+		 where 
+		  	id = 6;
+		  	SELECT date(NOW())
+		  	;
+		  	
+		  	INSERT INTO jang (
+		  		item_name
+		  		,amount
+		  		,d_day
+		  		,tag_id
+		  		,memo
+		  	)
+		  	VALUES (
+		  		'오토업뎃테스트'
+		  		,1
+		  		,20231011
+		  		,0
+		  		,'테슷후'
+		  	);
+		  	
+		 update 
+			jang 
+		 set 
+		  	finished = '1' 
+		  	,finished_at = date(now()) 
+		 where 
+		  	date(NOW()) > d_day 
+		;
