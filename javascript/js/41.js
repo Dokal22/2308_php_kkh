5b0fbc03ec582d2a67563221623c5a120a83dde6
Dokal22 <stly3466@gmail.com>
2023-10-27 18:03:25 +0900

    1027

--- javascript/js/41.js
+++ javascript/js/41.js
@@ -20,4 +20,20 @@
     
     // 2. clearTimeout(해당 setTimeout객체) : 타이머를 삭제
         let mySet = setTimeout(()=>console.log('No시간'), 6000);
-        clearTimeout(mySet);
+        // clearTimeout(mySet);
+
+    // 3. setInterval(콜백함수, 시간(ms)) : 일정 시간 만큼 반복
+        let myInter = setInterval(()=>console.log('허이짜'),500);
+
+    // 4. clearInterval(해당 setTimeout객체) : 인터버를 삭제
+        // clearInterval(myInter);
+
+        // 실습
+            // 오픈 5초후 두둥둥장
+                let h1 = document.createElement('h1')
+                setTimeout(()=> {
+                    h1.innerHTML='두두둥장'
+                    document.body.appendChild(h1)
+                    // const H1 = document.getElementsByTagName('h1');
+                    // H1[0].innerHTML='두두둥장' // 무친 배열놈
+                } , 7000);
