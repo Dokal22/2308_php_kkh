5f8215fef0144ec95e11804456a0d55fd5a5225d
Dokal22 <stly3466@gmail.com>
2023-10-30 17:43:47 +0900

    midium 숙제

---
+++ side_project/midium_board/src/header.php
@@ -0,0 +1,12 @@
+<div>
+    <div>
+        <span></span>
+        <span>Obsidian</span>
+    </div>
+    <div>
+        <span>Sign Up</span>
+        <span>Log In</span>
+        <span></span>
+        <span></span>
+    </div>
+</div>
---
+++ side_project/midium_board/src/list.php
@@ -0,0 +1,16 @@
+<?php
+define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/midium_board/src/");
+define("FILE_HEADER", ROOT."header.php");
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php // require_once(FILE_HEADER); ?>
+</body>
+</html>
