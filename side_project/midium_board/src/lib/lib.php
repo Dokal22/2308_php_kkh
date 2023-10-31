ccc5c5b5150e370affa1717ad788e973fb604e43
Dokal22 <stly3466@gmail.com>
2023-10-31 10:07:06 +0900

    +create +lib **xcopy 주의

--- side_project/midium_board/src/lib/lib.php
+++ side_project/midium_board/src/lib/lib.php
@@ -1,3 +1,14 @@
 <?php
+function PDO_in(&$conn) {
+    $host = "localhost";
+    $user = "root";
+    $name = "midium_board";
+    $pw = "php504";
+    $charset = "utf8mb4";
+    $dns = "mysql:host=".$host.";name=".$name.";charset=".$charset;
 
+    $option = [
+        PDO::ATTR_
+    ];
+}
 ?>
