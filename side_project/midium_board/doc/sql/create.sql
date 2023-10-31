1f3acd286427f4fec2e62ef95fb6e4814aa6d78e
Dokal22 <stly3466@gmail.com>
2023-10-31 10:09:39 +0900

    *create

--- side_project/midium_board/doc/sql/create.sql
+++ side_project/midium_board/doc/sql/create.sql
@@ -11,6 +11,23 @@ CREATE TABLE board (
 	,delete_at DATETIME 
 	);
 	
+-- 컬럼 추가
+ALTER TABLE board ADD COLUMN deleted_at DATETIME;
+	
+-- 테이블 수정하기
+ALTER board MODIFY COLUMN delete_at DATETIME; 
+
+-- 테이블 삭제
+DROP TABLE board;
+ALTER table board DROP COLUMN delete_at;
+
+-- 이 밑은 테스트
+-- 보기
+	-- 딜리트 값이 없는 친구들
+	SELECT * FROM board 
+	WHERE delete_at IS NULL;
+
+-- 입력하기
 -- 값 생성
 INSERT INTO board (
 	title
@@ -22,4 +39,16 @@ INSERT INTO board (
 	,NOW()
 );
 
+-- 수정하기
+UPDATE board
+SET 
+	delete_at = NULL
+WHERE 
+	id = 1;
+	
+-- 없애기
+DELETE FROM board
+WHERE id = 0;
+
 COMMIT;
+ROLLBACK;
