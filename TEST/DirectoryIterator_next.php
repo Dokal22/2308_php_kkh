2b14d44e3890c9c483fc5f0ca0f2d336549e55c4
Dokal22 <stly3466@gmail.com>
2024-04-13 00:36:38 +0900

    php, SplFileInfo 객체 test

--- TEST/DirectoryIterator_next.php
+++ TEST/DirectoryIterator_next.php
@@ -1,11 +1,20 @@
 <?php
-$iterator = new DirectoryIterator(dirname(__FILE__));
-var_dump($iterator);
-while ($iterator->valid()) {
-    $iterator->next();
-}
+// $iterator = new DirectoryIterator(dirname(__FILE__));
+// var_dump($iterator);
+// while ($iterator->valid()) {
+//     $iterator->next();
+// }
 
-foreach ($iterator as $entry) {
-    var_dump($entry);
-    $iterator->next();
+// foreach ($iterator as $entry) {
+//     var_dump($entry);
+//     $iterator->next();
+// }
+
+$fileInfo = new SplFileInfo(dirname(__FILE__) . '\DirectoryIterator_next.php');
+
+if ($fileInfo->isFile()) {
+    $inode = $fileInfo->getInode();
+    echo "File Inode: $inode\n";
+} else {
+    echo "The specified path is not a file.\n";
 }
